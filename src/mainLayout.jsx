409459bad173bd7ac1c6249c633d42fba62d5ba7
yangjaehyuk <gndksakxk10@naver.com>
2024-06-13 02:29:20 +0900

    fix: change mainlayout

--- src/mainLayout.jsx
+++ src/mainLayout.jsx
@@ -4,20 +4,45 @@ import MainHeader from './components/layouts/main-header';
 import styled from 'styled-components';
 import PropTypes from 'prop-types';
 import CustomFooter from './components/layouts/footer';
+
+const { Header, Content } = Layout;
+
 const MainLayout = ({ children }) => {
   return (
-    <Layout>
-      <MainHeader></MainHeader>
-      <StyledContent>{children}</StyledContent>
-      <CustomFooter />
-    </Layout>
+    <StyledLayout>
+      <Header>
+        <MainHeader />
+      </Header>
+      <Content>
+        <StyledContent>{children}</StyledContent>
+      </Content>
+      <Footer>
+        <CustomFooter />
+      </Footer>
+    </StyledLayout>
   );
 };
-const StyledContent = styled(Layout.Content)`
-  max-width: 1280px;
-  margin: 0 auto;
+
+const StyledLayout = styled(Layout)`
+  min-height: 100vh;
+  position: relative;
 `;
-export default MainLayout;
+
+const StyledContent = styled.div`
+  max-width: 100vw;
+  margin: 0 auto;
+  padding-bottom: 10vh;
+`;
+
+const Footer = styled.div`
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  width: 100%;
+`;
+
 MainLayout.propTypes = {
   children: PropTypes.node.isRequired,
 };
+
+export default MainLayout;
