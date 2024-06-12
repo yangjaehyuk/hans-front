57af96886567a8549f42bd75ffc23014c4fb4dd3
yangjaehyuk <gndksakxk10@naver.com>
2024-06-12 16:19:32 +0900

    feat: add sign-in and sign-up-agreement routes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,31 @@
-import './App.css';
+import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from './pages/home';
+import SignIn from './pages/sign-in';
+import SignInAgreement from './pages/sign-up-agreement';
+import AuthLayout from './authLayout';
 import { ROUTES } from './constants/routes';
-
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path={ROUTES.HOME} element={<Home />} />
+        <Route
+          path={ROUTES.SIGNIN}
+          element={
+            <AuthLayout title="">
+              <SignIn />
+            </AuthLayout>
+          }
+        />
+        <Route
+          path={ROUTES.SIGNIN_AGREEMENT}
+          element={
+            <AuthLayout title="서비스 이용 동의">
+              <SignInAgreement />
+            </AuthLayout>
+          }
+        ></Route>
       </Routes>
     </BrowserRouter>
   );
