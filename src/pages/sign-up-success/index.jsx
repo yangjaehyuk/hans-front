cc9a07055a62358160c5f1fbf5137d0cd3289de0
yangjaehyuk <gndksakxk10@naver.com>
2024-06-12 19:15:24 +0900

    feat: add rough sign-up-success design

---
+++ src/pages/sign-up-success/index.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import SignUpSuccessContainer from '../../components/sign-up-success';
+const SignUpSuccess = () => {
+  return <SignUpSuccessContainer />;
+};
+
+export default SignUpSuccess;
