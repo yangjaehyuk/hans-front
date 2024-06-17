eb4498fe002952b0ef057f5b589875a7adf5d607
yangjaehyuk <gndksakxk10@naver.com>
2024-06-18 00:03:12 +0900

    fix: change modify validate schema

--- src/utils/modify/validateSchema.js
+++ src/utils/modify/validateSchema.js
@@ -17,22 +17,10 @@ const ValidateSchema = () => {
       .string()
       .oneOf([yup.ref('password')], '비밀번호가 일치하지 않습니다.')
       .required('비밀번호를 다시 입력하세요.'),
-    file: yup
-      .mixed()
-      .required('파일을 업로드해주세요.')
-      .test('fileSize', '파일 크기는 최대 2MB 이하여야 합니다.', (value) =>
-        value ? value.size <= 2 * 1024 * 1024 : true,
-      )
-      .test(
-        'fileType',
-        '지원하는 파일 형식은 JPG, JPEG, GIF, PNG 입니다.',
-        (value) =>
-          value
-            ? ['image/jpg', 'image/jpeg', 'image/gif', 'image/png'].includes(
-                value.type,
-              )
-            : true,
-      ),
+    files: yup
+      .array()
+      .min(1, '최소 1개의 이미지를 업로드 해주세요.')
+      .required('파일을 업로드 해주세요.'),
   });
 };
 
