b4146510fce6fcb2aa9a110a332f4ea202493211
yangjaehyuk <gndksakxk10@naver.com>
2024-06-18 00:00:04 +0900

    feat: setting cookie

--- src/utils/cookie/index.js
+++ src/utils/cookie/index.js
@@ -1,17 +1,25 @@
 export const setCookie = (accessToken) => {
   try {
-    document.cookie = `accessToken=${accessToken};path=/;secure`;
+    document.cookie = `accessToken=${accessToken};path=/;max-age=300000`;
   } catch (e) {
     console.error(e);
   }
 };
 
 export function getCookie(name) {
-  const cookieValue = document.cookie
-    .split('; ')
-    .find((row) => row.startsWith(`${name}=`))
-    ?.split('=')[1];
-  return cookieValue || undefined;
+  const cookieString = document.cookie;
+  if (cookieString.length > 0) {
+    const cookies = cookieString.split('; ');
+    for (let i = 0; i < cookies.length; i++) {
+      const cookie = cookies[i];
+      const separatorIndex = cookie.indexOf('=');
+      const cookieName = cookie.substring(0, separatorIndex);
+      if (cookieName === name) {
+        return decodeURIComponent(cookie.substring(separatorIndex + 1));
+      }
+    }
+  }
+  return undefined;
 }
 
 export const deleteAllCookies = () => {
