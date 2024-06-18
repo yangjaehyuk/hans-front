a66c09e5c7bcd42b435a0dc8d9566eb60d8ea6db
yangjaehyuk <gndksakxk10@naver.com>
2024-06-19 01:46:00 +0900

    fix: change validation logic

--- src/pages/edit/index.jsx
+++ src/pages/edit/index.jsx
@@ -17,69 +17,102 @@ import styled from 'styled-components';
 import { colors } from '../../constants/colors';
 import ValidateSchema from '../../utils/post/validateSchema';
 import { TextBox } from '../../stores/atom/text-box';
+import PostAPI from '../../api/post-api';
+import { useParams } from 'react-router-dom';
 import { useCustomNavigate } from '../../hooks';
 const { TextArea } = Input;
-const { Dragger } = Upload;
-const arr = [
-  {
-    title: '김가을',
-    nickname: '양재혁',
-    body: 'dive into ive',
-    likesCount: 123,
-    tagList: ['김가을짱'],
-    imgList: [
-      'https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png',
-      'https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png',
-      'https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png',
-    ],
-    product: {
-      productPrice: 1239,
-      productImg:
-        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSMRs2OVbsbczIHMpdR0BGeMF8x1_fbHkZR5w&s',
-      productUrl: 'www.naver.com',
-      productName: '네이버',
-    },
-  },
-];
 
 const Edit = () => {
   const { handleChangeUrl } = useCustomNavigate();
+  const { postId } = useParams();
+  const [detailArr, setDetailArr] = useState({
+    title: '',
+    detail: '',
+    hashtags: [],
+    files: [],
+  });
+  const [isLoading, setIsLoading] = useState(true);
   const { token } = theme.useToken();
-
   const [inputVisible, setInputVisible] = useState(false);
   const [inputValue, setInputValue] = useState('');
   const inputRef = useRef(null);
-
   const [previewOpen, setPreviewOpen] = useState(false);
   const [previewImage, setPreviewImage] = useState('');
-
   const [isDisabled, setIsDisabled] = useState(true);
+  const formik = useFormik({
+    initialValues: {
+      title: '',
+      detail: '',
+      hashtags: [],
+      files: [],
+    },
+    validationSchema: ValidateSchema,
+    onSubmit: async (values) => {
+      try {
+        console.log(values.hashtags);
+        await PostAPI.modifyPostAPI({
+          postId: postId,
+          title: values.title,
+          body: values.detail,
+          tagList: values.hashtags,
+          imgList: values.files,
+        });
+        // window.location.href = `http://localhost:3000/detail/${postId}`;
+        handleChangeUrl('/style');
+      } catch (error) {
+        console.error(error);
+      }
+    },
+  });
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setIsLoading(true);
+      try {
+        const response = await PostAPI.viewPostDetailAPI(postId);
+        const data = response.data.data;
+        console.log(data);
+
+        setDetailArr(data);
+        formik.setValues({
+          title: data.title,
+          detail: data.body,
+          hashtags: data.tagList,
+          files: data.imgList,
+        });
+      } catch (error) {
+        console.error(error);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    fetchData();
+  }, [postId]);
 
   useEffect(() => {
     if (inputVisible) {
       inputRef.current?.focus();
     }
   }, [inputVisible]);
-  const formik = useFormik({
-    initialValues: {
-      title: arr[0].title,
-      detail: arr[0].body,
-      hashtags: arr[0].tagList,
-      files: arr[0].imgList.map((imgUrl, index) => ({
-        uid: `${index}-${Date.now()}`, // Ensure unique uid by combining index with timestamp
-        name: imgUrl.split('/').pop(),
-        status: 'done',
-        url: imgUrl,
-        type: 'image/png',
-        originFileObj: imgUrl,
-      })),
-    },
-    validationSchema: ValidateSchema,
-    onSubmit: (values) => {
-      const blobUrls = values.files.map((file) => file.blobUrl);
-    },
-  });
 
+  useEffect(() => {
+    if (
+      detailArr.title.length > 0 &&
+      detailArr.body.length > 0 &&
+      detailArr.tagList.length > 0 &&
+      detailArr.imgList.length > 0
+    ) {
+      setIsDisabled(false);
+    } else {
+      setIsDisabled(true);
+    }
+  }, [detailArr.title, detailArr.body, detailArr.tagList, detailArr.imgList]);
+
+  // console.log(detailArr.title);
+  // console.log(detailArr.body);
+  // console.log(detailArr.tagList);
+  // console.log(detailArr.imgList);
   const handleRemove = (file) => {
     const updatedFiles = formik.values.files.filter((f) => f.uid !== file.uid);
     formik.setFieldValue('files', updatedFiles);
@@ -94,8 +127,15 @@ const Edit = () => {
   };
 
   const handleInputConfirm = () => {
-    if (inputValue && !formik.values.hashtags.includes(inputValue)) {
-      const newTags = [...formik.values.hashtags, inputValue];
+    if (
+      inputValue &&
+      !formik.values.hashtags.some((tag) => tag.body === inputValue)
+    ) {
+      const newTag = {
+        tagId: formik.values.hashtags.length + 1,
+        body: inputValue,
+      };
+      const newTags = [...formik.values.hashtags, newTag];
       formik.setFieldValue('hashtags', newTags); // Update Formik state
     }
     setInputVisible(false);
@@ -107,16 +147,16 @@ const Edit = () => {
     formik.setFieldValue('hashtags', newTags); // Update Formik state
   };
 
-  const forMap = (tag, index) => (
-    <span key={`${tag}-${index}`}>
-      <Tag closable onClose={() => handleClose(tag)}>
-        {tag}
+  const forMap = (tagObject, index) => (
+    <span key={`${tagObject.tagId}-${index}`}>
+      <Tag closable onClose={() => handleClose(tagObject.tagId)}>
+        {tagObject.body}
       </Tag>
     </span>
   );
 
   const tagChild = formik.values.hashtags.map(forMap);
-
+  console.log(tagChild);
   const handlePreview = async (file) => {
     if (!file.url && !file.preview) {
       file.preview = await getBase64(file.originFileObj);
@@ -126,26 +166,31 @@ const Edit = () => {
   };
 
   const handleChange = ({ fileList }) => {
-    const validTypes = ['image/jpeg', 'image/png', 'image/gif'];
-
-    if (
-      fileList.length > 0 &&
-      !validTypes.includes(fileList[fileList.length - 1].type)
-    ) {
-      message.error({
-        content: (
-          <TextBox typography="body4" fontWeight={'400'}>
-            지원하는 파일 형식은 JPG, JPEG, GIF, PNG 입니다.
-          </TextBox>
-        ),
-        duration: 2,
-        style: {
-          width: '346px',
-          height: '41px',
-        },
-      });
-    } else {
+    // console.log(fileList[fileList.length - 1].type);
+    if (fileList[fileList.length - 1].type === undefined) {
       formik.setFieldValue('files', fileList);
+    } else {
+      const validTypes = ['image/jpeg', 'image/png', 'image/gif'];
+
+      if (
+        fileList.length > 0 &&
+        !validTypes.includes(fileList[fileList.length - 1].type)
+      ) {
+        message.error({
+          content: (
+            <TextBox typography="body4" fontWeight={'400'}>
+              지원하는 파일 형식은 JPG, JPEG, GIF, PNG 입니다.
+            </TextBox>
+          ),
+          duration: 2,
+          style: {
+            width: '346px',
+            height: '41px',
+          },
+        });
+      } else {
+        formik.setFieldValue('files', fileList);
+      }
     }
   };
 
@@ -164,191 +209,197 @@ const Edit = () => {
       reader.onerror = (error) => reject(error);
     });
 
-  useEffect(() => {
-    console.log(formik.values.files);
-    if (
-      formik.values.title.length > 0 &&
-      formik.values.detail.length > 0 &&
-      formik.values.hashtags.length > 0 &&
-      formik.values.files.length > 0
-    ) {
-      setIsDisabled(false);
-    } else setIsDisabled(true);
-  }, [
-    formik.values.title,
-    formik.values.detail,
-    formik.values.hashtags,
-    formik.values.files,
-  ]);
-
   return (
     <StyledLayout>
-      <StyledContent>
-        <TextContainer style={{ cursor: 'default' }}>
-          <TextBox typography="h1" fontWeight="700">
-            Edit
-          </TextBox>
-        </TextContainer>
-        <FormContainer onSubmit={formik.handleSubmit}>
-          <TextBox variant="body2" fontWeight={'400'} cursor="default">
-            Title
-          </TextBox>
-          <EmailInner>
-            <EmailInput>
-              <StyledInput
-                size="large"
-                placeholder="제목 입력"
-                type="text"
-                name="title"
-                value={formik.values.title}
-                onChange={formik.handleChange}
-                onBlur={formik.handleBlur}
-                style={{
-                  borderColor:
-                    formik.touched.title && formik.errors.title
-                      ? 'red'
-                      : '#d9d9d9',
-                }}
-              />
-              {formik.touched.title && formik.errors.title && (
-                <TextBox typography="body4" fontWeight={'400'} color="error">
-                  {formik.errors.title}
-                </TextBox>
-              )}
-            </EmailInput>
-          </EmailInner>
-          <TextBox variant="body2" fontWeight={'400'} cursor="default">
-            Detail
-          </TextBox>
-          <EmailInner>
-            <EmailInput>
-              <StyledTextArea
-                size="large"
-                placeholder="내용 입력"
-                name="detail"
-                value={formik.values.detail}
-                onChange={formik.handleChange}
-                onBlur={formik.handleBlur}
-                style={{
-                  borderColor:
-                    formik.touched.detail && formik.errors.detail
-                      ? 'red'
-                      : '#d9d9d9',
-                }}
-              />
-              {formik.touched.detail && formik.errors.detail && (
-                <TextBox typography="body4" fontWeight={'400'} color="error">
-                  {formik.errors.detail}
-                </TextBox>
-              )}
-            </EmailInput>
-          </EmailInner>
-          <TextBox variant="body2" fontWeight={'400'} cursor="default">
-            HashTags
-          </TextBox>
-          <EmailInner>
-            <EmailInput>
-              <div style={{ marginBottom: 16 }}>
-                <TweenOneGroup
-                  name="hashtags"
+      {isLoading ? (
+        <LoadingContainer>
+          <BlackSpin size="large" />
+        </LoadingContainer>
+      ) : (
+        <StyledContent>
+          <TextContainer style={{ cursor: 'default' }}>
+            <TextBox typography="h1" fontWeight="700">
+              Edit
+            </TextBox>
+          </TextContainer>
+          <FormContainer onSubmit={formik.handleSubmit}>
+            <TextBox variant="body2" fontWeight={'400'} cursor="default">
+              Title
+            </TextBox>
+            <EmailInner>
+              <EmailInput>
+                <StyledInput
+                  size="large"
+                  placeholder="제목 입력"
+                  type="text"
+                  name="title"
+                  value={formik.values.title}
+                  onChange={formik.handleChange}
+                  onBlur={formik.handleBlur}
                   style={{
+                    borderColor:
+                      formik.touched.title && formik.errors.title
+                        ? 'red'
+                        : '#d9d9d9',
+                  }}
+                />
+                {formik.touched.title && formik.errors.title && (
+                  <TextBox typography="body4" fontWeight={'400'} color="error">
+                    {formik.errors.title}
+                  </TextBox>
+                )}
+              </EmailInput>
+            </EmailInner>
+            <TextBox variant="body2" fontWeight={'400'} cursor="default">
+              Detail
+            </TextBox>
+            <EmailInner>
+              <EmailInput>
+                <StyledTextArea
+                  size="large"
+                  placeholder="내용 입력"
+                  name="detail"
+                  value={formik.values.detail}
+                  onChange={formik.handleChange}
+                  onBlur={formik.handleBlur}
+                  style={{
+                    borderColor:
+                      formik.touched.detail && formik.errors.detail
+                        ? 'red'
+                        : '#d9d9d9',
+                  }}
+                />
+                {formik.touched.detail && formik.errors.detail && (
+                  <TextBox typography="body4" fontWeight={'400'} color="error">
+                    {formik.errors.detail}
+                  </TextBox>
+                )}
+              </EmailInput>
+            </EmailInner>
+            <TextBox variant="body2" fontWeight={'400'} cursor="default">
+              HashTags
+            </TextBox>
+            <EmailInner>
+              <EmailInput>
+                <div style={{ marginBottom: 16 }}>
+                  <TweenOneGroup
+                    name="hashtags"
+                    style={{
+                      display: 'flex',
+                      flexWrap: 'wrap',
+                      maxWidth: '100%',
+                    }}
+                    key={formik.values.hashtags
+                      .map((tag) => tag.tagId)
+                      .join(',')}
+                    enter={{
+                      scale: 0.8,
+                      opacity: 1,
+                      type: 'from',
+                      duration: 100,
+                    }}
+                    leave={{ opacity: 0, width: 0, scale: 0, duration: 200 }}
+                    onEnd={(e) => {
+                      if (e.type === 'appear' || e.type === 'enter') {
+                        e.target.style = 'display: inline-block';
+                      }
+                    }}
+                  >
+                    {tagChild}
+                  </TweenOneGroup>
+                </div>
+                {inputVisible ? (
+                  <Input
+                    ref={inputRef}
+                    type="text"
+                    size="small"
+                    style={{ width: 78 }}
+                    value={inputValue}
+                    onChange={handleInputChange}
+                    onBlur={handleInputConfirm}
+                    onPressEnter={handleInputConfirm}
+                  />
+                ) : (
+                  <Tag onClick={showInput} style={{ width: 78 }}>
+                    <PlusOutlined /> New Tag
+                  </Tag>
+                )}
+              </EmailInput>
+            </EmailInner>
+            <TextBox variant="body2" fontWeight={'400'} cursor="default">
+              Images
+            </TextBox>
+            <div>
+              <div style={{ width: '50vw' }}>
+                <Upload
+                  name="files"
+                  listType="picture-card"
+                  fileList={formik.values.files}
+                  onPreview={handlePreview}
+                  onChange={handleChange}
+                  onBlur={formik.handleBlur}
+                  onRemove={handleRemove}
+                  beforeUpload={() => false}
+                  style={{
+                    margin: '0 auto',
                     display: 'flex',
-                    flexWrap: 'wrap',
-                    maxWidth: '100%',
-                  }}
-                  key={formik.values.hashtags.join(',')}
-                  enter={{
-                    scale: 0.8,
-                    opacity: 1,
-                    type: 'from',
-                    duration: 100,
-                  }}
-                  leave={{ opacity: 0, width: 0, scale: 0, duration: 200 }}
-                  onEnd={(e) => {
-                    if (e.type === 'appear' || e.type === 'enter') {
-                      e.target.style = 'display: inline-block';
-                    }
+                    flexWrap: 'wrap !important',
                   }}
                 >
-                  {tagChild}
-                </TweenOneGroup>
+                  {uploadButton}
+                </Upload>
+                {previewImage && (
+                  <Image
+                    wrapperStyle={{ display: 'none' }}
+                    preview={{
+                      visible: previewOpen,
+                      onVisibleChange: (visible) => setPreviewOpen(visible),
+                      afterClose: () => setPreviewImage(''),
+                    }}
+                    src={previewImage}
+                  />
+                )}
               </div>
-              {inputVisible ? (
-                <Input
-                  ref={inputRef}
-                  type="text"
-                  size="small"
-                  style={{ width: 78 }}
-                  value={inputValue}
-                  onChange={handleInputChange}
-                  onBlur={handleInputConfirm}
-                  onPressEnter={handleInputConfirm}
-                />
-              ) : (
-                <Tag onClick={showInput} style={{ width: 78 }}>
-                  <PlusOutlined /> New Tag
-                </Tag>
-              )}
-            </EmailInput>
-          </EmailInner>
-          <TextBox variant="body2" fontWeight={'400'} cursor="default">
-            Images
-          </TextBox>
-          <div>
-            <div style={{ width: '50vw' }}>
-              <Upload
-                name="files"
-                listType="picture-card"
-                fileList={formik.values.files}
-                onPreview={handlePreview}
-                onChange={handleChange}
-                onBlur={formik.handleBlur}
-                onRemove={handleRemove}
-                beforeUpload={() => false}
-                style={{
-                  margin: '0 auto',
-                  display: 'flex',
-                  flexWrap: 'wrap !important',
-                }}
-              >
-                {uploadButton}
-              </Upload>
-              {previewImage && (
-                <Image
-                  wrapperStyle={{ display: 'none' }}
-                  preview={{
-                    visible: previewOpen,
-                    onVisibleChange: (visible) => setPreviewOpen(visible),
-                    afterClose: () => setPreviewImage(''),
-                  }}
-                  src={previewImage}
-                />
-              )}
             </div>
-          </div>
 
-          <ButtonContainer>
-            <StyledDoneButton
-              htmlType="submit"
-              disabled={isDisabled}
-              type="primary"
-            >
-              <TextBox
-                typography="h5"
-                fontWeight={'700'}
-                textAlign="center"
-                color="white"
+            <ButtonContainer>
+              <StyledDoneButton
+                htmlType="submit"
+                disabled={isDisabled}
+                type="primary"
               >
-                완료
-              </TextBox>
-            </StyledDoneButton>
-          </ButtonContainer>
-        </FormContainer>
-      </StyledContent>
+                <TextBox
+                  typography="h5"
+                  fontWeight={'700'}
+                  textAlign="center"
+                  color="white"
+                >
+                  완료
+                </TextBox>
+              </StyledDoneButton>
+            </ButtonContainer>
+          </FormContainer>
+        </StyledContent>
+      )}
     </StyledLayout>
   );
 };
+const LoadingContainer = styled.div`
+  display: flex;
+  height: 100vh;
+  width: 100vw;
+  justify-content: center;
+  align-items: center;
+  margin: 0 auto;
+`;
 
+const BlackSpin = styled(Spin)`
+  .ant-spin-dot {
+    i {
+      background: black;
+    }
+  }
+`;
 const StyledLayout = styled(Layout)`
   max-width: 100vw;
   background-color: white;
